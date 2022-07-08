bd6f88dfa9cf1755f26dfa284b5f1369688c40e0
Cris Jacobo <crisjofficial@gmail.com>
2022-07-07 23:58:11 -0500

    completed exercise

--- JSInfo/task_12.js
+++ JSInfo/task_12.js
@@ -1,1 +1,29 @@
-functui
+function unique(arr)
+{
+
+}
+
+function unique(arr) {
+    for (let i = 0; i < arr.length; i++)
+    {
+        let temp = arr[i];
+
+        let tempArr = [...arr];
+
+        tempArr.splice(i,1);
+
+        if (tempArr.indexOf(temp) >= 0)
+        {
+            arr.splice(i,1)
+            i--
+        }
+    }
+
+    return arr
+  }
+  
+  let strings = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O", "ki", "ki"
+  ];
+  
+  console.log( unique(strings) ); // Hare, Krishna, :-O
