631d7f20689d0ddefc2db1764e4d8ff952b0a9a4
Cris Jacobo <crisjofficial@gmail.com>
2022-07-08 23:52:40 -0500

    completed exercise

--- JSInfo/task_11.js
+++ JSInfo/task_11.js
@@ -1,1 +1,14 @@
-functui
+function getAverageAge(users)
+{
+    let sum = users.reduce((a,c) => a + c.age, 0)
+
+    return sum / users.length;
+}
+
+let john = { name: "John", age: 20 };
+let pete = { name: "Pete", age: 40 };
+let mary = { name: "Mary", age: 60 };
+
+let arr = [ john, pete, mary ];
+
+console.log( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
