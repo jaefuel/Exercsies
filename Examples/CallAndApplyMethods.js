fadcdfc31c51f96d986b6997c7464f1667f06611
Cris Jacobo <crisjofficial@gmail.com>
2023-03-31 16:49:39 -0500

    added call and apply method examples

---
+++ Examples/CallAndApplyMethods.js
@@ -0,0 +1,20 @@
+class Food
+{
+    constructor(name, price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+
+
+}
+
+function toString(store)
+{
+    console.log(`"You purchased a ${this.name} for $${this.price} at ${store}`);
+}
+
+toString.call(new Food("Papaya",7.50), "H-E-B");
+
+toString.apply(new Food("Papaya",7.50), ["H-E-B"]);
+
